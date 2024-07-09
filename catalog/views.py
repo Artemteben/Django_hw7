6659fea4927c85d4b90044e9c01a105683704ea9
Artemteben <artyom.teben@yandex.ru>
2024-07-09 23:25:01 +0300

    dj

---
+++ catalog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def index():
+    return None
