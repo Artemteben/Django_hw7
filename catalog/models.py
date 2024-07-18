a133cf74570d633f5481d51d89591e313a011d26
Artemteben <artyom.teben@yandex.ru>
2024-07-18 21:11:19 +0300

    mk migr

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование категории")
+    description = models.TextField(
+        verbose_name="Описание категории", blank=True, null=True
+    )
+
+
+def __str__(self):
+    return f"{self.name} {self.description}"
+
+
+class Meta:
+    verbose_name = "категория"
+    verbose_name_plural = "категории"
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование"
+    )
+    description = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Описание",
+        blank=True,
+        null=True,
+        related_name="products"
+    )
+    image = models.ImageField(
+        max_length=150, verbose_name="Изображение", blank=True, null=True
+    )
+    category = models.CharField(max_length=150, verbose_name="Категория")
+    price = models.IntegerField(verbose_name="Цена за покупку")
+    created_at = models.DateField(max_length=150, verbose_name="Дата создания")
+    updated_at = models.DateField(
+        max_length=150, verbose_name="Дата последнего изменения"
+    )
+
+    def __str__(self):
+        return (
+            f"{self.name}/n"
+            f" {self.description}/n"
+            f" {self.image}/n"
+            f" {self.category}/n"
+            f"{self.price}/n"
+            f" {self.created_at}/n"
+            f"{self.updated_at}"
+        )
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
--- catalog/views.py
+++ catalog/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
         message = request.POST.get("message")
         print(name, phone, message)
-    return render(request, 'contacts.html')
+    return render(request, "contacts.html")
 
 
 def index():
