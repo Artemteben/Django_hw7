07aad626cf00661cc54b6334bd16b553d3ea8510
Artemteben <artyom.teben@yandex.ru>
2024-07-27 22:01:54 +0300

    cssjs_and_other2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from catalog.apps import CatalogConfig
-from catalog.views import contacts, home, base_r
+from catalog.views import contacts, home, base_r, product_detail
 
 app_name = CatalogConfig.name
 
-urlpatterns = [path('', base_r, name='product_list')]
+urlpatterns = [
+    path('', base_r, name='product_list'),
+    path('products/<int:pk>/', product_detail, name='product_detail')
+]
+
 # path("", home, name="home"),
 # path("contacts/", contacts, name="contacts"),
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -20,3 +20,9 @@ def base_r(request):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "product_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context1 = {'product': product}
+    return render(request, 'product_detail.html', context1)
